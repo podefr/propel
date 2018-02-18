c3d2b1454069ffc6a607671a005f096ed6277186
Ryan Dahl <ry@tinyclouds.org>
2018-02-17 19:41:05 -0500

    Add tensor.linear

--- src/tensor.ts
+++ src/tensor.ts
@@ -12,10 +12,12 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import { range } from "./api";
+import { isUndefined } from "util";
+import { randn, range, zeros } from "./api";
 import { bo, convertBasic } from "./backend";
 import * as format from "./format";
 import * as ops from "./ops";
+import { Params } from "./params";
 import * as types from "./types";
 import { allFinite, assert, assertShapesEqual } from "./util";
 
@@ -571,6 +573,48 @@ export class Tensor implements types.BasicTensor {
                .mul(outScale[1] - outScale[0])
                .add(outScale[0]);
   }
+
+  /** Returns x*w+b for the input tensor x.
+   * Where w ("weights") and b ("bias") looked up in params.
+   * If the params object doesn't contain these parameters, they are
+   * initialized.
+   *
+   * If the input tensor has shape [d0, d1, d2, ... ] then it will be reshaped
+   * to [d0, d1 * d2 * ...] before applying the matmul. That means you can use
+   * 4D image tensors with this function without having to reshape it.
+   *
+   * If users are calling this function multiple times, they should use
+   * params.scope("myLayerName") to not conflict with other params.
+   *
+   *    import { params, zeros } from "propel";
+   *    params = params();
+   *    inputs = zeros([2, 5]);
+   *    outputs = inputs.linear(params.scope("L1"), 10);
+   *    params.has("L1/weights") && params.has("L1/bias");
+   */
+  linear(params: Params, outDim: number, opts: LinearOpts = {}): Tensor {
+    assert(this.rank >= 2);
+    // Set default options if not set.
+    if (isUndefined(opts.bias)) opts.bias = true;
+    if (isUndefined(opts.scale)) opts.scale = 0.01;
+    // Partially flatten tensor if needed.
+    let t = this.rank === 2 ? this : this.reshape([this.shape[0], -1]);
+    const inDim = t.shape[t.rank - 1];
+    const w = params.init("weights", () =>
+      randn([inDim, outDim]).mul(opts.scale));
+    t = t.matmul(w);
+    if (opts.bias) {
+      const b = params.init("bias", () => zeros([outDim]));
+      t = t.add(b);
+    }
+    return t;
+  }
+}
+
+interface LinearOpts {
+  bias?: boolean;
+  scale?: number;
+  // TODO custom initializers.
 }
 
 /** Like range() but outputs a javascript array of numbers. */
